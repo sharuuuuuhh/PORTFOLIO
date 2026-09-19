b776903ac3a6fefcef2e3f8ca518bd10551ffebf
sharuuuuuhh <sharuuuuuhh@gmail.com>
2026-09-20 01:55:10 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/PORTFOLIO",
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "**" }
-    ]
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
